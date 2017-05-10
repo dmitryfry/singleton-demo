00e5d23009bdc81881d2c9c631e9fb1a37d0067c
Dmitry Fry <im@dmitryfry.com>
2017-05-10 14:46:56 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -2,6 +2,14 @@ class Logger
   def self.say_something
     puts 'lala'
   end
+
+  def self.log_something wat
+    f = File.open 'log.txt', 'a'
+    f.puts wat
+    f.close
+  end
 end
 
 Logger.say_something
+
+Logger.log_something 'wazuup'
