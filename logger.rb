a428ef5d882f03b116aeace70423d92fa88791d0
Dmitry Fry <im@dmitryfry.com>
2017-05-10 16:25:52 +0300

    add default ruby Singltone

--- logger.rb
+++ logger.rb
@@ -1,28 +1,16 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
-
-  def self.instance
-    # if @@x == nil
-    #   @@x = Logger.new
-    # end
-
-    return @@x
-  end
-  # class method
-  def self.say_something
-    puts 'lala'
-  end
-
-  # instance method
   def log_something wat
 
     @f.puts wat
 
   end
 
-  private_class_method :new
 end
